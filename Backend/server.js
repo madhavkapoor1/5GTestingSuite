85e1f35628444947f037d431ea79b3e5cab9054e
madhavkapoor1 <madhavkapoor2001@gmail.com>
2024-08-12 14:29:40 -0700

    express.js backend setup

---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const PORT = 4000
+
+//Temp Database
+const db = [];
+
+//Middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(require('cors')());
+
+//Routes
+app.get('/', (req, res) => {
+
+    console.log('Request received for GET'); 
+    res.sendStatus(200)
+});
+
+app.post('/api/info', (req, res) => {
+    
+    const { information } = req.body;
+    console.log('The posted request was: ', information);  
+    db.push(information);
+    console.log('DB', db);
+    res.status(201).json({"YourMessage": information});
+});
+
+app.put('/api', (req, res) => {    
+    // const { information } = req.body;
+    // console.log(information); 
+    const {word, banana} = req.query;
+    console.log(word, banana); 
+    res.status(200).send('Hello World!');
+});
+
+app.delete('/delete/:id', (req, res) => {
+
+    console.log('Request received for DELETE'); 
+
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));   
